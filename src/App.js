ed7887715a8bab7788172d242d133b01832cca5d
Adam Sori <adam.sori@gmail.com>
2022-03-29 00:34:20 -0300

    Lifting State Up from NE to App

--- src/App.js
+++ src/App.js
@@ -35,9 +35,15 @@ const App = () => {
       date: new Date(2021, 5, 15),
     },
   ];
+
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,31 +1,111 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
 
     const titleChangeHandler = (event) => {
-        console.log(event.target.value);
-    }
+        setEnteredTitle(event.target.value);
+    };
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+    };
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+    };
 
-    return <form>
-        <div className='new-expense__constrols'>
-            <div className='new-expense__control'>
-                <label>Title</label>
-                <input type='text' onChange={titleChangeHandler} />
+
+    /*
+    THIS IS AN ALTERNATIVE FOR THE CODE AT THE BEGINNING
+    const [userInput, setUserInput] = useState({
+        enteredTitle: '',
+        enteredAmount: '',
+        enteredDate: ''
+    });
+
+    const titleChangeHandler = (event) => {
+        // In most cases this will work!
+        // But React schedules state updates, it does not perform instantly
+        // And therefore, if I use this aproach with a lot of state updates, we could end with outdated or incorrect information
+        // setUserInput({
+        //     ...userInput, // Spread Operator
+        //     enteredTitle: event.target.value,
+        // })
+
+
+        // This is the safe way, to garatee that we will aways will be using the preview state
+        setUserInput((prevState) => {
+            return { ...prevState, enteredTitle: event.target.value };
+        });
+
+    };
+    const amountChangeHandler = (event) => {
+        setUserInput({
+            ...userInput,
+            enteredAmount: event.target.value,
+        })
+    };
+    const dateChangeHandler = (event) => {
+        setUserInput({
+            ...userInput,
+            enteredDate: event.target.value,
+        })
+    };
+    */
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const expenseData = {
+            title: enteredTitle, // enteredTitle points at the state const [enteredTitle, setEnteredTitle] = useState('');
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+        props.onSaveExpenseData(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    };
+
+
+    return (
+        <form onSubmit={submitHandler}>
+            <div className='new-expense__constrols'>
+                <div className='new-expense__control'>
+                    <label>Title</label>
+                    <input
+                        type='text'
+                        value={enteredTitle}
+                        onChange={titleChangeHandler}
+                    />
+                </div>
+                <div className='new-expense__control'>
+                    <label>Amount</label>
+                    <input
+                        type='number'
+                        value={enteredAmount}
+                        min="0.01"
+                        step="0.01"
+                        onChange={amountChangeHandler}
+                    />
+                </div>
+                <div className='new-expense__control'>
+                    <label>Date</label>
+                    <input
+                        type='date'
+                        value={enteredDate}
+                        min="2019-01-01"
+                        max="2022-12-31"
+                        onChange={dateChangeHandler}
+                    />
+                </div>
             </div>
-            <div className='new-expense__control'>
-                <label>Amount</label>
-                <input type='number' min="0.01" step="0.01" />
+            <div className='new-expense__actions'>
+                <button type="submit">Add Expense</button>
             </div>
-            <div className='new-expense__control'>
-                <label>Date</label>
-                <input type='date' min="2019-01-01" max="2022-12-31" />
-            </div>
-        </div>
-        <div className='new-expense__actions'>
-            <button type="submit">Add Expense</button>
-        </div>
-    </form>
+        </form>
+    )
 };
 
 export default ExpenseForm;
